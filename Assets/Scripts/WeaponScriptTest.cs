f5d73350fdc01dab2669f77df7a092c88a14a7a4
Stanislav Maltsev <stasmaltsev@mail.ru>
2023-11-16 17:17:57 +0200

    16.11.2023
    Throw item, while jumping. Player stats started

--- Assets/Scripts/WeaponScriptTest.cs
+++ Assets/Scripts/WeaponScriptTest.cs
@@ -1,4 +1,5 @@
 using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 
 public class WeaponScriptTest : MonoBehaviour
@@ -8,12 +9,14 @@ public class WeaponScriptTest : MonoBehaviour
     [SerializeField] private Rigidbody2D weaponBody;
     [SerializeField] private Rigidbody2D shadowBody;
     [SerializeField] private Transform player;
+    [SerializeField] private Transform playerSpriteAnimation;
     [SerializeField] private CharacterMovement characterMovement;
     [SerializeField] private SpriteRenderer weaponSpriteRenderer;
     [SerializeField] private SpriteRenderer shadowSpiteRenderer;
     private bool isSpriteOrder;
     public bool isThrow;
     public bool isPickUp;
+    public bool isJumpingWithWeapon;
     private void Start()
     {
         isSpriteOrder = true;
@@ -22,45 +25,71 @@ public class WeaponScriptTest : MonoBehaviour
         PickObjectsTest.ThrowItem += ThrowObject;
         PickObjectsTest.OnPickUp += PickUp;
 
-        Observable.EveryUpdate().Subscribe(_ => 
+        Observable.EveryUpdate().Subscribe(_ =>
         {
             Stay();
             SpriteBalance();
+            JumpWithWeapon();
+            //SpriteEnemyBalance();
         });
     }
 
-    public void ThrowObject()
+    private void JumpWithWeapon()
     {
-        if(!isThrow) { return; }
-        if (player.rotation == Quaternion.Euler(0, 0, 0))
+        if(characterMovement.isJumping && isJumpingWithWeapon)
         {
-            shadowSpiteRenderer.enabled = true;
-            transform.SetParent(null);
-            pipe.SetParent(shadow) ;
-            isSpriteOrder = true ;
-            shadowBody.bodyType = RigidbodyType2D.Dynamic;
-            weaponBody.bodyType = RigidbodyType2D.Dynamic;
-            weaponBody.AddForce(new Vector2(3,1f) * 4, ForceMode2D.Impulse);
-            shadowBody.AddForce(new Vector2(3,0) * 4, ForceMode2D.Impulse);
-        }
+            pipe.SetParent(playerSpriteAnimation);
+            isThrow = false;
 
-       else
+        }
+        if(!isPickUp && isJumpingWithWeapon && !characterMovement.isJumping)
         {
+            pipe.SetParent(player);
+            isThrow = true;
+            pipe.localPosition = new Vector2(1.4f, 2.56f);
+        }
+        if (Input.GetKeyDown(KeyCode.K) && characterMovement.isJumping)
+        {
+            Debug.Log("Throw");
+            isPickUp = true;
+            isJumpingWithWeapon = false;
             shadowSpiteRenderer.enabled = true;
             transform.SetParent(null);
             pipe.SetParent(shadow);
-            isSpriteOrder = false ;
-            shadowBody.bodyType = RigidbodyType2D.Dynamic;
-            weaponBody.bodyType = RigidbodyType2D.Dynamic;
-            weaponBody.AddForce(new Vector2(3, -1f) * -4, ForceMode2D.Impulse);
-            shadowBody.AddForce(new Vector2(3, 0) * -4, ForceMode2D.Impulse);
-        }
-    }
+            isSpriteOrder = true;
 
+
+            //weaponBody.AddForce(new Vector2(3, -1f) * 2, ForceMode2D.Impulse);
+            //shadowBody.AddForce(new Vector2(3, 0) * 2, ForceMode2D.Impulse);
+            ThrowWeaponDirection(new Vector2(3,1), new Vector2(3,0), 2);
+        }
+        //if (characterMovement.isJumping && isPickUp)
+        //{
+
+        //    isSpriteOrder = false;
+        //    weaponBody.bodyType = RigidbodyType2D.Kinematic;
+        //    shadowBody.bodyType = RigidbodyType2D.Kinematic;
+        //    pipe.SetParent(playerSpriteAnimation);
+        //    shadow.SetParent(playerSpriteAnimation);
+        //    weaponSpriteRenderer.sortingOrder = 1;
+        //    shadowSpiteRenderer.enabled = false;
+        //    pipe.localPosition = new Vector2(1.4f, 2.56f);
+        //    shadow.transform.localPosition = new Vector2(1.4f, 0);
+        //    FlipWeapon();
+        //}
+        //else if(!characterMovement.isJumping && isPickUp)
+        //{
+        //    pipe.SetParent(null);
+        //    shadow.SetParent(null);
+        //   // pipe.localPosition = new Vector2(1.4f, 2.56f);
+        //}
+    }
     public void PickUp()
     {
-        if(!isPickUp) { return; }
+        if (!isPickUp) { return; }
         isSpriteOrder = false;
+        isJumpingWithWeapon = true;
+        isPickUp = false;
         weaponBody.bodyType = RigidbodyType2D.Kinematic;
         shadowBody.bodyType = RigidbodyType2D.Kinematic;
         pipe.SetParent(player);
@@ -71,6 +100,37 @@ public class WeaponScriptTest : MonoBehaviour
         shadow.transform.localPosition = new Vector2(1.4f, 0);
         FlipWeapon();
     }
+    public void ThrowObject()
+    {
+        if (!isThrow) { return; }
+        if (player.rotation == Quaternion.Euler(0, 0, 0))
+        {
+            shadowSpiteRenderer.enabled = true;
+            isJumpingWithWeapon = false;
+            transform.SetParent(null);
+            pipe.SetParent(shadow);
+            isSpriteOrder = true;
+            isPickUp = true;
+            shadowBody.bodyType = RigidbodyType2D.Dynamic;
+            weaponBody.bodyType = RigidbodyType2D.Dynamic;
+            weaponBody.AddForce(new Vector2(3, 1f) * 4, ForceMode2D.Impulse);
+            shadowBody.AddForce(new Vector2(3, 0) * 4, ForceMode2D.Impulse);
+        }
+
+        else
+        {
+            shadowSpiteRenderer.enabled = true;
+            transform.SetParent(null);
+            pipe.SetParent(shadow);
+            isSpriteOrder = false;
+            isPickUp = true;
+            shadowBody.bodyType = RigidbodyType2D.Dynamic;
+            weaponBody.bodyType = RigidbodyType2D.Dynamic;
+            weaponBody.AddForce(new Vector2(3, -1f) * -4, ForceMode2D.Impulse);
+            shadowBody.AddForce(new Vector2(3, 0) * -4, ForceMode2D.Impulse);
+        }
+    }
+
 
     private void Stay()
     {
@@ -97,15 +157,44 @@ public class WeaponScriptTest : MonoBehaviour
 
     private void SpriteBalance()
     {
-        if(!isSpriteOrder) { return; }
+        if (!isSpriteOrder) { return; }
         if (characterMovement.isJumping && player.position.y < transform.position.y)
         {
             weaponSpriteRenderer.sortingOrder = -10;
         }
-        else if (characterMovement.isJumping == false)
+        else if (!characterMovement.isJumping)
         {
             weaponSpriteRenderer.sortingOrder = 0;
         }
     }
 
+    private void SpriteEnemyBalance()
+    {
+        if (transform.position.y > ((gameObject.GetComponent(typeof(EnemyAI))).transform.position.y))
+        {
+            weaponSpriteRenderer.sortingOrder = -10;
+        }
+        else
+        {
+            weaponSpriteRenderer.sortingOrder = 0;
+        }
+    }
+
+    private void ThrowWeaponDirection(Vector2 pipeThrowDirection, Vector2 shadowThrowDirection, float throwForce)
+    {
+        if(player.transform.rotation.y < 0)
+        {
+            shadowBody.bodyType = RigidbodyType2D.Dynamic;
+            weaponBody.bodyType = RigidbodyType2D.Dynamic;
+            weaponBody.AddForce(pipeThrowDirection * -throwForce, ForceMode2D.Impulse);
+            shadowBody.AddForce(shadowThrowDirection * -throwForce, ForceMode2D.Impulse);
+        }
+        else
+        {
+            shadowBody.bodyType = RigidbodyType2D.Dynamic;
+            weaponBody.bodyType = RigidbodyType2D.Dynamic;
+            weaponBody.AddForce(pipeThrowDirection * throwForce, ForceMode2D.Impulse);
+            shadowBody.AddForce(shadowThrowDirection * throwForce, ForceMode2D.Impulse);
+        }
+    }
 }
